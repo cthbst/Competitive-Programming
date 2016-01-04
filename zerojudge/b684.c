92003fe921f4dc833eb42a00c4e12e04bef8be4e
cthbst <edison1998402@gmail.com>
2016-01-04 19:05:40 +0800

    Create b684.c

---
+++ zerojudge/b684.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int pos[1000030] ;
+
+int main(){
+    int n ; scanf("%d",&n ) ;
+
+    int i ,pi ;
+    for (i=0 ,pi ;i<n ;i++ ){
+        scanf("%d",&pi) ;
+        pos[pi-1]=i ;
+    }
+
+    int vec=1 ,ans=0 ;
+
+    for (i=1 ;i<n ;i++ ){
+        if ((pos[i]-pos[i-1])*vec<0){
+            ans++ ;
+            vec*=-1 ;
+        }
+    }
+
+    printf("%d\n",ans ) ;
+}
