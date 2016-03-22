1ee4290d290cf450c4d90ff9dd17d45490fb4dcf
cthbst <edison1998402@gmail.com>
2016-03-22 19:41:54 +0800

    Create pD.cpp

---
+++ contest/TOI2016/T1/pD.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+typedef unsigned long long LLU ;
+typedef long long LL ;
+
+LLU bound ;
+LLU sol(LLU x ,LLU X ,LLU Y ){
+    if (x>X)return 0 ;
+    if (x==0)return (Y+1)+sol(x+1,X,Y) ;
+    LLU cx=min(bound/x,Y) ,cX=min(bound/X,Y) ;
+    if (cx==cX)return (X-x+1)*(cx+1) ;
+    LLU m = (x+X)/2 ;
+    return sol(x,m,Y)+sol(m+1,X,Y) ;
+}
+
+int m ;
+char cmd ;
+LL L ,R ;
+
+
+int main(){
+    cin >>m >>cmd >>L >>R ;
+    LLU ans = (LLU)(R-L+1)*(LLU)(R-L+1) ;
+    if (cmd=='s'){
+        if (L<0 && R<0 )L=-L ,R=-R ,swap(R,L) ;
+        if (L>=0){
+            bound = (1LL<<(m-1))-1 ;
+            ans -= sol(0,R,R) ;
+            if (L>0)ans += sol(0,L-1,R)*2 ;
+            if (L>0)ans -= sol(0,L-1,L-1) ;
+        }
+        else {
+            bound = (1LL<<(m-1))-1 ;
+            ans -= sol(0,R,R) ;
+            ans -= sol(0,-L,-L) ;
+            bound++ ;
+            ans -= sol(0,-L,R)*2 ;
+            ans += (R-L+1)*2+1 ;
+        }
+    }
+    else { //cmd=='u'
+        bound = (1LL<<m)-1 ;
+        ans -= sol(0,R,R) ;
+        if (L>0)ans += sol(0,L-1,R)*2 ;
+        if (L>0)ans -= sol(0,L-1,L-1) ;
+    }
+    cout << ans <<endl ;
+}
