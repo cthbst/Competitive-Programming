35a431efe2c3266a9b9e7f73a83b16da21cedca5
cthbst <edison1998402@gmail.com>
2016-05-08 17:43:12 +0800

    Create a221.cpp

---
+++ zerojudge/a221.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+string s1 ,s2 ;
+
+string noneSpace(string s ){
+    string re ;
+    for (int i=0 ;i<s.size() ;i++ ){
+        char c = s[i] ;
+        if (c!=' ') re+=c ;
+    }
+    return re ;
+}
+
+void solve(){
+    getline(cin,s1) ;
+    getline(cin,s2) ;
+    if (s1==s2)cout << "Yes" ;
+    else if (noneSpace(s1)==noneSpace(s2))cout <<"Output Format Error" ;
+    else cout <<"Wrong Answer";
+}
+
+int main(){
+    int T ; cin >> T ;
+    getline(cin,s1) ;
+    for (int t=1 ;t<=T ;t++ ){
+        cout <<"Case "<<t <<": " ;
+        solve() ;
+        cout <<endl ;
+    }
+}
