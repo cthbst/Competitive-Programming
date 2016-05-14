6c57fcbfe233322db81d0d86fdfbbf2d204a6629
cthbst <edison1998402@gmail.com>
2016-05-14 11:51:07 +0800

    Create a009.cpp

---
+++ zerojudge/a009.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+
+int main(){
+    string s ;
+    while (getline(cin,s)){
+        for (int i=0 ;i<s.size() ;i++ ){
+            s[i]+= '*'-'1' ;
+        }
+        cout << s <<endl ;
+    }
+}
