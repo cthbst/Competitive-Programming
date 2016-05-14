b3e80750beab0406e7026a5a08948a32b2e2c141
cthbst <edison1998402@gmail.com>
2016-05-14 17:14:32 +0800

    Create a015.cpp

---
+++ zerojudge/a015.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std ;
+
+int main(){
+    const int maxn = 100 ;
+    int n ,m ;
+    int A[maxn][maxn] ;
+
+    while (cin >> n >> m ){
+        for (int i=0 ;i<n ;i++ ){
+            for (int j=0 ;j<m ;j++ ){
+                cin >> A[i][j] ;
+            }
+        }
+
+        for (int j=0 ;j<m ;j++ ){
+            for (int i=0 ;i<n ;i++ ){
+                cout << A[i][j] <<" " ;
+            }
+            cout << endl ;
+        }
+    }
+}
