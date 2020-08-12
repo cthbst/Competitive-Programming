d67726dbba2e956fd18136e6686756b08db9540b
Tsung-Ta Wu <edison1998402@gmail.com>
2020-08-12 16:39:51 +0800

    Update a002.cpp

--- zerojudge/a002.cpp
+++ zerojudge/a002.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv) {
-    int a, b, sum;
+int main() {
+    int a, b;
     while (cin >> a >> b) {
-        sum = a + b;
-        cout << sum << endl;
+        cout << a + b << endl;
     }
     return 0;
 }
