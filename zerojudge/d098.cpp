ffd02542f8fdfdf18dcd9ecaaf30dd561f717953
Tsung-Ta Wu <edison1998402@gmail.com>
2020-08-13 10:23:41 +0800

    Update d098.cpp

--- zerojudge/d098.cpp
+++ zerojudge/d098.cpp
@@ -1,40 +1,35 @@
-#include <stdio.h>
-#include <string.h>
-
 #include <iostream>
 #include <sstream>
 #include <string>
 using namespace std;
 
-char in[10000];
-bool is_number(const char *s) {
-    if ('0' <= s[0] && s[0] <= '9' || s[0] == '-') {
-        for (int i = 0; s[i]; i++) {
-            if ('0' <= s[i] && s[i] <= '9')
-                continue;
-            else
-                return 0;
+bool is_number(const string &s) {
+    for (int i = 0; s[i]; i++) {
+        if (!isdigit(s[i])) {
+            return false;
         }
-        return 1;
-    } else
-        return 0;
+    }
+    return true;
 }
-int main() {
-    while (cin.getline(in, sizeof(in))) {
-        stringstream s;
-        s << in;
-        int sum = 0;
-        string x;
 
-        while (s >> x) {
-            if (is_number(x.c_str())) {
-                stringstream ss;
-                ss << x;
-                int add;
-                ss >> add;
-                sum += add;
-            }
+int eval(const string &s) {
+    if (is_number(s)) {
+        return stoi(s);
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+    string line;
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        int sum = 0;
+        for (string x; ss >> x;) {
+            sum += eval(x);
         }
-        printf("%d\n", sum);
+        cout << sum << '\n';
     }
 }
