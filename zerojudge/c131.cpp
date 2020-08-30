e475ce3615b08fc480cd4fef5b1e8e09bdcd1234
Tsung-Ta Wu <edison1998402@gmail.com>
2020-08-31 07:40:41 +0800

    Add c131

---
+++ zerojudge/c131.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> 
+    for (int cs = 1, u, v; cin >> u >> v; cs++) {
+        if (u == -1 && v == -1) break;
+        edges.push_back({u, v});
+    }
+    return 0;
+}
